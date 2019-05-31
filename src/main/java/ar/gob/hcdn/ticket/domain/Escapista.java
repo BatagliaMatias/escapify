29b0ba82f6562dab9e2a08bbcca5b1a019da615c
mbataglia <matubat91@gmail.com>
2019-05-31 20:46:03 -0300

    db

---
+++ src/main/java/ar/gob/hcdn/ticket/domain/Escapista.java
@@ -0,0 +1,36 @@
+package ar.gob.hcdn.ticket.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Escapista")
+public class Escapista {
+    private String nombre;
+    private String apellido;
+    private String usuario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
