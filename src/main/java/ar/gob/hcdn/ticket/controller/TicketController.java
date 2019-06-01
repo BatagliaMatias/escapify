78cdf58fb159429e3b0322f1698523bef6507fec
Matías Bataglia <matubat91@gmail.com>
2019-06-01 00:48:21 -0300

    raygun

--- src/main/java/ar/gob/hcdn/ticket/controller/TicketController.java
+++ src/main/java/ar/gob/hcdn/ticket/controller/TicketController.java
@@ -1,6 +1,5 @@
 package ar.gob.hcdn.ticket.controller;
 
-import ar.gob.hcdn.ticket.common.ErrorDeServicioDTO;
 import ar.gob.hcdn.ticket.common.HcdnResponse;
 import ar.gob.hcdn.ticket.dto.UserDTO;
 import io.swagger.annotations.Api;
@@ -28,10 +27,7 @@ public class TicketController {
 	@ApiOperation(value = "Devuelve los datos del usuario ", notes = "Conecta al CRM para pedir los datos del usuario")
 	public ResponseEntity<HcdnResponse<UserDTO>> findUser(@PathVariable("userID") String userID,
 			@RequestHeader(required = false, name = "Mail-Auth") String auth) {
-		long start = System.currentTimeMillis();
-		try {
-			// TODO: Generar service para autorizacion!, revisar si se utiliza lo mismo que
-			// con emails
+
 			HcdnResponse<UserDTO> response = new HcdnResponse<UserDTO>();
 			UserDTO userDTO = new UserDTO();
 			userDTO.setNombre("DUMMY");
@@ -39,16 +35,7 @@ public class TicketController {
 			userDTO.setUserID("1");
 			response.setData(userDTO);
 			return new ResponseEntity<HcdnResponse<UserDTO>>(response, HttpStatus.OK);
-		} catch (Exception e) {
-			HcdnResponse<UserDTO> response = new HcdnResponse<UserDTO>(new ErrorDeServicioDTO(Long.valueOf("00654"),
-					e.getLocalizedMessage(), "Se produjo un error invocando al servicio"));
-			return new ResponseEntity<HcdnResponse<UserDTO>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-		} finally {
-			long end = System.currentTimeMillis();
 
-			logger.info(
-					new StringBuilder("Tiempo findTickets(userID) [").append(end - start).append("] MS").toString());
-		}
 	}
 
 
