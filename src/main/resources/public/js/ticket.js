5700046e8a767dfbf4b7e074acb09a3cf5dd1600
mbataglia <matubat91@gmail.com>
2019-06-21 17:19:31 -0300

    login con google

--- src/main/resources/public/js/ticket.js
+++ src/main/resources/public/js/ticket.js
@@ -151,11 +151,15 @@ angular
 						console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 					}
 
+					window.onSignIn = onSignIn;
+
 					function signOut() {
 						var auth2 = gapi.auth2.getAuthInstance();
 						auth2.signOut().then(function () {
 							console.log('User signed out.');
 						});
 					}
+
+					window.signOut = signOut;
 
 				});
